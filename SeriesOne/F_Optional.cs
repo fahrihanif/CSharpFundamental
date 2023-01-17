fb17dc5f86b8514763ef501f7466ecb659d0c36c
Fahri Hanif <fahrihanif03@gmail.com>
2023-01-17 11:44:25 +0700

    Update F_Optional.cs

--- SeriesOne/F_Optional.cs
+++ SeriesOne/F_Optional.cs
@@ -30,6 +30,6 @@ public class F_Optional
         Console.WriteLine("Ketikan : " + num + ".");
 
         // String interpolation
-        Console.WriteLine($"Ketikan : num.");
+        Console.WriteLine($"Ketikan : {num}.");
     }
 }
